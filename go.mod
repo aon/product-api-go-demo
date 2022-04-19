c356d27516ae091ef29634a572a5e9cbbedcc2fe
aon <21188659+aon@users.noreply.github.com>
2022-04-18 21:27:20 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module microservice-go-demo
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"log"
+	"microservice-go-demo/handlers"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+
+	// create the handlers
+	ph := handlers.NewProducts(l)
+
+	// create new serve mux and register the handlers
+	sm := http.NewServeMux()
+	sm.Handle("/", ph)
+
+	// create a new server
+	s := &http.Server{
+		Addr:         ":9091",           // configure the bind address
+		Handler:      sm,                // set the default handler
+		ErrorLog:     l,                 // set the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP keep-alive
+	}
+
+	// start the server
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	// trap sigterm or interrupt and gracefully shutdown the server
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	// block until a signal is received
+	sig := <-c
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
+}
